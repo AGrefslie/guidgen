4082c0fef2b4379bdb9130787f62fd046bc88537
Axel Grefslie <agrefs97@gmail.com>
2025-11-28 14:45:16 +0100

    Initial Commit

---
+++ GuidGen/GuidGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidGenApp.swift
+//  GuidGen
+//
+//  Created by Axel Grefslie on 28/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct GuidGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuidGenTests/GuidGenTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuidGenTests.swift
+//  GuidGenTests
+//
+//  Created by Axel Grefslie on 28/11/2025.
+//
+
+import Testing
+@testable import GuidGen
+
+struct GuidGenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
